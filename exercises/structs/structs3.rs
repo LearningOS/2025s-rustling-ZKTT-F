4a96e437dc3b9b0d962279164208d1dec8100154
KKTT9 <machinemind@foxmail.com>
2025-03-31 11:14:39 +0000

    update

--- exercises/structs/structs3.rs
+++ exercises/structs/structs3.rs
@@ -7,8 +7,6 @@
 // Execute `rustlings hint structs3` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
-
 #[derive(Debug)]
 struct Package {
     sender_country: String,
@@ -29,12 +27,14 @@ impl Package {
         }
     }
 
-    fn is_international(&self) -> ??? {
+    fn is_international(&self) -> bool {
         // Something goes here...
+        return self.recipient_country != self.sender_country;
     }
 
-    fn get_fees(&self, cents_per_gram: i32) -> ??? {
+    fn get_fees(&self, cents_per_gram: i32) -> i64 {
         // Something goes here...
+        return (cents_per_gram as i64 * self.weight_in_grams as i64);
     }
 }
 
